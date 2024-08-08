5d903bf2daa717945c19e86c44b9069c58a988b0
Artem Gluhovskiy <gluhovskiy.art@gmail.com>
2024-08-08 21:19:41 +0200

    Shuffle an Array

---
+++ misc/src/main/java/org/art/playground/misc/algo/design/p1_ShuffleArray.java
@@ -0,0 +1,53 @@
+package org.art.playground.misc.algo.design;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Given an integer array nums, design an algorithm to randomly shuffle the array.
+ * All permutations of the array should be equally likely as a result of the shuffling.
+ */
+public class p1_ShuffleArray {
+
+    static class Solution {
+
+        private static final ThreadLocalRandom TLR = ThreadLocalRandom.current();
+
+        private int[] nums;
+
+        public Solution(int[] nums) {
+            this.nums = nums;
+        }
+
+        public int[] reset() {
+            return this.nums;
+        }
+
+        public int[] shuffle() {
+            int[] copy = new int[nums.length];
+
+            System.arraycopy(this.nums, 0, copy, 0, this.nums.length);
+
+            for (int i = 0; i < copy.length; i++) {
+                int currentVal = copy[i];
+                int rnd = TLR.nextInt(nums.length);
+                copy[i] = copy[rnd];
+                copy[rnd] = currentVal;
+            }
+
+            return copy;
+        }
+    }
+
+    @Test
+    void test0() {
+        var s = new Solution(new int[]{1, 2, 3});
+        System.out.println(Arrays.toString(s.reset()));
+        System.out.println(Arrays.toString(s.shuffle()));
+        System.out.println(Arrays.toString(s.reset()));
+        System.out.println(Arrays.toString(s.shuffle()));
+    }
+
+}
