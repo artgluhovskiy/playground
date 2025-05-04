1b6c396cd05100f67288f2e49ba4c98ee6f90b4b
Artsiom Hlukhouski <artsiomh@playtika.com>
2025-05-04 17:38:39 +0200

    Happy Number

---
+++ misc/src/main/java/org/art/playground/misc/algo/math/e_5_HappyNumber.java
@@ -0,0 +1,47 @@
+package org.art.playground.misc.algo.math;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class e_5_HappyNumber {
+
+    public boolean isHappy(int n) {
+        Set<Integer> visited = new HashSet<>();
+
+        while (!visited.contains(n)) {
+            visited.add(n);
+
+            n = sumSquared(n);
+            if (n == 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int sumSquared(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int val = n % 10;
+            sum = sum + (int) Math.pow(val, 2);
+            n = n / 10;
+        }
+        return sum;
+    }
+
+    @Test
+    void test0() {
+        assertThat(isHappy(19)).isTrue();
+    }
+
+    @Test
+    void test1() {
+        assertThat(isHappy(2)).isFalse();
+    }
+}
+
